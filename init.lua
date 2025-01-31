8ba0edd9308574fb54a052f607a46065ed6d23d4
Gavin Estenssoro <gav.estenssoro@gmail.com>
2025-01-30 19:42:39 -0700

    :stop() function

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 ---
 --- Display UTC time in the menu bar
 ---
---- Download:
+--- Download: [https://github.com/gavinest/ZeroOffset/blob/main/Spoons/ZeroOffset.spoon.zip](https://github.com/gavinest/ZeroOffset/blob/main/Spoons/ZeroOffset.spoon.zip)
 
 local obj={}
 obj.__index = obj
@@ -14,12 +14,20 @@ obj.author = "Gavin Estenssoro"
 obj.homepage = "https://github.com/gavinest/ZeroOffset"
 obj.license = "MIT - https://opensource.org/licenses/MIT"
 
+obj.menuBarItem = nil
 obj.showUtc = false
 
 function obj:start()
+    if self.menuBarItem then self:stop() end
     self.menuBarItem = hs.menubar.new()
     self.menuBarItem:setClickCallback(function() self:menuItemClicked() end)
     self:toggleShowUtc()
+    return self
+end
+
+function obj:stop()
+    if self.menuBarItem then self.menuBarItem:delete() end
+    self.menuBarItem = nil
     return self
 end
 
